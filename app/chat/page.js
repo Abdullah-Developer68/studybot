9823aa543ab09a14cfd77eccea46b9a4ce277baf
Abdullah <abdullahdeveloper843@gmail.com>
2025-12-07 18:06:29 +0500

    Formed the structure of the chat section.

---
+++ app/chat/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Input from "@/components/chat/Input";
+const Chat = () => {
+  return (
+    <>
+      <div className="h-screen w-full flex justify-center">
+        <Input />
+      </div>
+    </>
+  );
+};
+
+export default Chat;
