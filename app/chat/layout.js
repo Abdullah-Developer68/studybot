62918483a7780e167a464aded71260dcd72746bc
Abdullah <abdullahdeveloper843@gmail.com>
2025-12-08 23:15:56 +0500

    moved the side to the layout file of the chat page.

---
+++ app/chat/layout.js
@@ -0,0 +1,25 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/global/AppSidebar";
+import { cookies } from "next/headers";
+import Chat from "./page";
+
+const ChatLayout = async () => {
+  // This is a feature of next.js that allows to access cookies on the server side during SSR.
+  const cookieStore = await cookies();
+  // It gets the data from the request headers
+  const defaultOpen = cookieStore.get("sidebar-state")?.value === "true";
+
+  return (
+    <>
+      <SidebarProvider defaultOpen={defaultOpen}>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          <Chat />
+        </main>
+      </SidebarProvider>
+    </>
+  );
+};
+
+export default ChatLayout;
--- app/chat/page.js
+++ app/chat/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Input from "@/components/chat/Input";
+
 const Chat = () => {
   return (
     <>
