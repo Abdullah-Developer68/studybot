e3d5406c68fd00e6013537b53ddcff1cdfe3dfb5
Abdullah <abdullahdeveloper843@gmail.com>
2025-12-29 01:24:12 +0500

    Improved the chat interface. Next step is to modify the shadcn components like input for the chat.

--- app/chat/page.js
+++ app/chat/page.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Input from "@/components/chat/Input";
+import ChatBox from "@/components/chat/ChatBox";
 
 const Chat = () => {
   return (
     <>
-      <div className="h-screen w-full flex justify-center">
-        <Input />
+      <div className="relative h-screen w-full flex flex-col items-center justify-center">
+        <span className="">
+          <ChatBox />
+        </span>
+        <span className="relative top-70 w-full flex justify-center items-center">
+          <Input />
+        </span>
       </div>
     </>
   );
--- components/chat/Input.js
+++ components/chat/Input.js
@@ -24,32 +24,11 @@ import {
 
 const Input = () => {
   return (
-    <div className="grid w-full max-w-sm gap-6">
+    <div className="grid w-full max-w-sm h-fit gap-6">
       <InputGroup>
-        <InputGroupInput placeholder="Search..." />
-        <InputGroupAddon>
-          <Search />
-        </InputGroupAddon>
-        <InputGroupAddon align="inline-end">12 results</InputGroupAddon>
-      </InputGroup>
-      <InputGroup>
-        <InputGroupInput placeholder="example.com" className="!pl-1" />
-        <InputGroupAddon>
-          <InputGroupText>https://</InputGroupText>
-        </InputGroupAddon>
-        <InputGroupAddon align="inline-end">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <InputGroupButton className="rounded-full" size="icon-xs">
-                <IconInfoCircle />
-              </InputGroupButton>
-            </TooltipTrigger>
-            <TooltipContent>This is content in a tooltip.</TooltipContent>
-          </Tooltip>
-        </InputGroupAddon>
-      </InputGroup>
-      <InputGroup>
-        <InputGroupTextarea placeholder="Ask, Search or Chat..." />
+        <InputGroupTextarea
+          placeholder="Ask, Search or Chat..."
+        />
         <InputGroupAddon align="block-end">
           <InputGroupButton
             variant="outline"
@@ -73,7 +52,7 @@ const Input = () => {
             </DropdownMenuContent>
           </DropdownMenu>
           <InputGroupText className="ml-auto">52% used</InputGroupText>
-          <Separator orientation="vertical" className="!h-4" />
+          <Separator orientation="vertical" className="h-4" />
           <InputGroupButton
             variant="default"
             className="rounded-full"
@@ -85,16 +64,8 @@ const Input = () => {
           </InputGroupButton>
         </InputGroupAddon>
       </InputGroup>
-      <InputGroup>
-        <InputGroupInput placeholder="@shadcn" />
-        <InputGroupAddon align="inline-end">
-          <div className="bg-primary text-primary-foreground flex size-4 items-center justify-center rounded-full">
-            <IconCheck className="size-3" />
-          </div>
-        </InputGroupAddon>
-      </InputGroup>
     </div>
   );
-}
+};
 
 export default Input;
