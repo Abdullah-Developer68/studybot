7ee15aa34fc246de770a0e1e1bbef28eb3224672
Abdullah <abdullahdeveloper843@gmail.com>
2026-01-18 20:30:38 +0500

    feat: update NewTemplate component to include a user-friendly markdown editor

--- app/templates/page.js
+++ app/templates/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TemplateManager from "@/components/templates/templateManager";
+import TemplateManager from "@/components/templates/TemplateManager";
 
 const Template = () => {
   return <TemplateManager />;
