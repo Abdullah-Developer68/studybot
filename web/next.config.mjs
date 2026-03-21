ba0770106617ca778325cbb6a8fcbb170a109cfb
Abdullah <abdullahdeveloper843@gmail.com>
2026-03-22 02:00:37 +0500

    While moving off to typescript Next.js shifted to using the StudyBot as the current directory. Now, it has been set to web manually in the next.config.mjs file.

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,9 +1,18 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+// gets the paths where the next.config.mjs file is located, which is the root of the project
+const currentDir = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
   // Transpile local workspace package consumed from node_modules
   transpilePackages: ["@studybot/supabase", "@studybot/utils"],
+  turbopack: {
+    root: currentDir,
+  },
   experimental: {
     // This allows importing from outside the 'web' directory
     externalDir: true,
