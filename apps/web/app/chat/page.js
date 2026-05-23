e03cbcb8474775ace5bb6c32162e3cea2359fdee
Abdullah <abdullahdeveloper843@gmail.com>
2026-05-23 23:50:26 +0500

    For now the chat page is the default later it will be a landing page.

--- apps/web/app/chat/page.js
+++ apps/web/app/chat/page.js
@@ -1,5 +1,18 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import Input from "@/components/chat/Input";
+import ChatBox from "@/components/chat/ChatBox";
+import ChatProvider from "@/app/providers/ChatProvider";
 
 export default function ChatPage() {
-  redirect("/");
+  return (
+    <ChatProvider>
+      <div className="relative flex h-screen w-full flex-col overflow-hidden">
+        <ChatBox />
+        <span className="fixed bottom-5 left-1/2 w-3/4 -translate-x-1/2 flex items-center justify-center">
+          <Input />
+        </span>
+      </div>
+    </ChatProvider>
+  );
 }
--- apps/web/app/page.js
+++ apps/web/app/page.js
@@ -1,22 +1,5 @@
-"use client";
-import React from "react";
-import Input from "@/components/chat/Input";
-import ChatBox from "@/components/chat/ChatBox";
-import ChatProvider from "@/app/providers/ChatProvider";
+import { redirect } from "next/navigation";
 
-const Chat = () => {
-  return (
-    <>
-      <ChatProvider>
-        <div className="relative flex h-screen w-full flex-col overflow-hidden">
-          <ChatBox />
-          <span className="fixed bottom-5 left-1/2 w-3/4 -translate-x-1/2 flex justify-center items-center">
-            <Input />
-          </span>
-        </div>
-      </ChatProvider>
-    </>
-  );
-};
-
-export default Chat;
+export default function HomePage() {
+  redirect("/chat");
+}
